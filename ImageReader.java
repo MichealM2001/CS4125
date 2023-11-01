837507661fd5c79d2bf9fa6205d62ae79a8606be
Rian <rianboleary@gmail.com>
2023-11-01 15:42:47 +0000

    Create ImageReader
    Created an image reader class.
    Will be used for submitting an image of an id.

---
+++ ImageReader.java
@@ -0,0 +1,25 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageReader {
+    
+    public BufferedImage readImage(String pathName) throws IOException{
+        File imageFile = new File(pathName);
+        BufferedImage image = ImageIO.read(imageFile);
+        return image;
+    }
+
+    /** Testing 
+    * public static void main(String[] args) {
+    *     ImageReader imageReader = new ImageReader();
+    *    try{
+    *        BufferedImage image = imageReader.readImage("C:\\Users\\Rian\\Desktop\\Year 3 Sem2\\devlink\\DevLink.JPG");
+    *    }catch (IOException e){
+    *        e.printStackTrace();
+    *    }
+    * }
+ */
+}
