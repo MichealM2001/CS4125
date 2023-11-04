bd8151f65cd845d4d503751a5627451d64242f58
Mahir Jha (20266766) <126410705+mahirjha81@users.noreply.github.com>
2023-11-04 19:20:48 +0000

    Update AddCar.java
    Add classification

--- AddCar.java
+++ AddCar.java
@@ -12,13 +12,14 @@ public class AddCar {
         Scanner scanner = new Scanner(System.in); // Initializing a scanner to take input from the user
 
         try (PrintWriter pw = new PrintWriter(fileName)) {
-            pw.write("Year of registration" + "," + "Car model" + "," + "Price" + "," + "Rented\n"); // First row of text in CSV indicating what each column means
+            pw.write("Year of registration" + "," + "Car model" + "," + "Price" + "," + "Rented" + "," + "Classification\n"); // First row of text in CSV indicating what each column means
             
             while (true) { //Starts loops without an actual condition. Just assumes it to be true to start loop
                 int yearOfReg; // Declaring a variable that holds car's year of registration
                 String model; // Declaring a variable that holds car's model
                 int price; //Declaring a variable that holds car's price
-                String isRented; //Declaring a variable that holds car's rental status 
+                String isRented; //Declaring a variable that holds car's rental status
+                String classification; //Declaring a variable that holds car's classification
 
                 System.out.println("Enter year of registration."); // Message asking user to enter year of car's registration
                 yearOfReg = scanner.nextInt(); // Using input as car's year of registration
@@ -35,8 +36,11 @@ public class AddCar {
                 
                 System.out.println("Is car rented?"); // Message asking the user to enter car's rental status
                 isRented = scanner.nextLine(); //Using input as car's rental status
+                
+                System.out.println("Enter classification of car"); //Message asking the user to enter car's classification
+                classification = scanner.nextLine(); //Using input as car's classification
 
-                pw.write(yearOfReg + "," + model + "," + price + "," + isRented + "\n"); // Writing values taken from user input into CSV
+                pw.write(yearOfReg + "," + model + "," + price + "," + isRented + "," + classification +"\n"); // Writing values taken from user input into CSV
 
                 System.out.println("Car details added to the CSV."); //Confirms that details have been added to CSV
                 System.out.println("Do you want to add another car? (Type 'yes' or 'no')");
