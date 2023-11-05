8ea350700e9dc56f4a0633adca41914f9b03790f
Mahir Jha (20266766) <126410705+mahirjha81@users.noreply.github.com>
2023-11-05 15:52:42 +0000

    Update AddCar.java
    Removed function for reading CSV file to its own class

--- AddCar.java
+++ AddCar.java
@@ -1,15 +1,11 @@
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.FileReader;
-import java.io.BufferedReader;
 import java.util.Scanner;
 
 public class AddCar {
     public static void main(String[] args) {
         String fileName = "AddCar.csv"; // Creating the name of the CSV file
         writeToCSVFile(fileName); // Calling the function that writes to AddCar.csv
-        readCSVFile(fileName); //Calling the function that reads AddCar.csv
     }
 
     private static void writeToCSVFile(String fileName) {
@@ -58,24 +54,6 @@ public class AddCar {
             System.out.println("Finished writing to file"); // Final message
         } catch (FileNotFoundException e) {
             System.out.println("Error creating/writing to the file.");
-            e.printStackTrace();
-        }
-    }
-    
-    private static void readCSVFile(String filename){ //Creating function to read CSV file with data of cars
-        try(BufferedReader br = new BufferedReader(new FileReader(filename))){
-            
-            String line = "";
-            
-            while ( (line = br.readLine()) != null ) {
-                System.out.println(line);
-            }
-            
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            e.printStackTrace();
-        } catch (IOException e){
-            System.out.println("File could not be closed");
             e.printStackTrace();
         }
     }
