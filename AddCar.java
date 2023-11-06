6166050d0bd24db10c7efeb3f940fad7e91ad02c
BarryFoles <91254622+BarryFoles@users.noreply.github.com>
2023-11-06 10:50:22 +0000

    Update AddCar.java

--- AddCar.java
+++ AddCar.java
@@ -1,59 +1,102 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class AddCar {
     public static void main(String[] args) {
-        String fileName = "AddCar.csv"; // Creating the name of the CSV file
-        writeToCSVFile(fileName); // Calling the function that writes to AddCar.csv
+        String fileName = "AddCar.csv"; // Define the name of the CSV file
+        Scanner scanner = new Scanner(System.in); // Create a scanner to read user input
+
+        // Main loop to allow the user to choose an operation
+        while (true) {
+            System.out.println("Choose an operation:");
+            System.out.println("1. Add a car");
+            System.out.println("2. Remove a car");
+            System.out.println("3. Exit");
+            int choice = scanner.nextInt(); // Read the user's choice
+            scanner.nextLine(); // Consume the newline character
+
+            if (choice == 1) {
+                writeToCSVFile(fileName, scanner); // Call the function to add a car
+            } else if (choice == 2) {
+                removeCarFromCSV(fileName, scanner); // Call the function to remove a car
+            } else if (choice == 3) {
+                break; // Exit the program
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 
-    private static void writeToCSVFile(String fileName) {
-        Scanner scanner = new Scanner(System.in); // Initializing a scanner to take input from the user
+    private static void writeToCSVFile(String fileName, Scanner scanner) {
+        try (FileWriter fw = new FileWriter(fileName, true)) {
+            // Prompt the user for car details
+            System.out.println("Enter year of registration:");
+            int yearOfReg = scanner.nextInt();
+            scanner.nextLine();
 
-        try (PrintWriter pw = new PrintWriter(fileName)) {
-            pw.write("Year of registration" + "," + "Car model" + "," + "Price" + "," + "Rented" + "," + "Classification\n"); // First row of text in CSV indicating what each column means
-            
-            while (true) { //Starts loops without an actual condition. Just assumes it to be true to start loop
-                int yearOfReg; // Declaring a variable that holds car's year of registration
-                String model; // Declaring a variable that holds car's model
-                int price; //Declaring a variable that holds car's price
-                String isRented; //Declaring a variable that holds car's rental status
-                String classification; //Declaring a variable that holds car's classification
+            System.out.println("Enter model of car:");
+            String model = scanner.nextLine();
 
-                System.out.println("Enter year of registration."); // Message asking user to enter year of car's registration
-                yearOfReg = scanner.nextInt(); // Using input as car's year of registration
-                
-                scanner.nextLine(); // Moves to next line
+            System.out.println("Enter price of car:");
+            int price = scanner.nextInt();
+            scanner.nextLine();
 
-                System.out.println("Enter model of car."); // Message asking the user to enter car's model
-                model = scanner.nextLine(); // Using input as car's model
-                
-                System.out.println("Enter price of car."); // Message asking the user to enter car's price
-                price = scanner.nextInt(); //Using input as car's price
-                
-                scanner.nextLine(); //Moves to next line
-                
-                System.out.println("Is car rented?"); // Message asking the user to enter car's rental status
-                isRented = scanner.nextLine(); //Using input as car's rental status
-                
-                System.out.println("Enter classification of car"); //Message asking the user to enter car's classification
-                classification = scanner.nextLine(); //Using input as car's classification
+            System.out.println("Is car rented? (yes or no):");
+            String isRented = scanner.nextLine().toLowerCase();
 
-                pw.write(yearOfReg + "," + model + "," + price + "," + isRented + "," + classification +"\n"); // Writing values taken from user input into CSV
+            System.out.println("Enter classification of car:");
+            String classification = scanner.nextLine();
 
-                System.out.println("Car details added to the CSV."); //Confirms that details have been added to CSV
-                System.out.println("Do you want to add another car? (Type 'yes' or 'no')");
-                
-                String response = scanner.nextLine().toLowerCase(); //Takes user's response and converts text to lower case so that response is not case sensitive
-                if (!response.equals("yes")) {
-                    break; // Exits the loop if user is done.
+            // Write the car details to the CSV file
+            fw.write(yearOfReg + "," + model + "," + price + "," + isRented + "," + classification + "\n");
+            System.out.println("Car details added to the CSV.");
+        } catch (IOException e) {
+            System.out.println("Error creating/writing to the file.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void removeCarFromCSV(String fileName, Scanner scanner) {
+        try {
+            File inputFile = new File(fileName);
+            File tempFile = new File("temp.csv");
+
+            if (!inputFile.exists()) {
+                System.out.println("File not found.");
+                return;
+            }
+
+            // Prompt the user for the car model to remove
+            System.out.println("Enter the model of the car to remove:");
+            String carModelToRemove = scanner.nextLine();
+
+            Scanner fileScanner = new Scanner(inputFile);
+            FileWriter fw = new FileWriter(tempFile, true);
+
+            // Iterate through the lines in the input CSV file
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(",");
+
+                // Check if the line contains at least two parts and if the car model matches
+                if (parts.length >= 2 && !parts[1].equalsIgnoreCase(carModelToRemove)) {
+                    fw.write(line + "\n"); // Write the line to the temporary file
                 }
             }
 
-            System.out.println("Finished writing to file"); // Final message
-        } catch (FileNotFoundException e) {
-            System.out.println("Error creating/writing to the file.");
+            fileScanner.close();
+            fw.close();
+
+            // Replace the original CSV file with the temporary file
+            if (inputFile.delete() && tempFile.renameTo(inputFile)) {
+                System.out.println("Car removed from the CSV.");
+            } else {
+                System.out.println("Failed to remove car.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading/writing to the file.");
             e.printStackTrace();
         }
     }
