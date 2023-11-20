00d98535207de30eff736575b0f23b6ca89019c0
rian-ol <64734797+rian-ol@users.noreply.github.com>
2023-11-20 12:39:51 +0000

    Updated repairstate
    added in functionality for repaircar

--- States/RepairState.java
+++ States/RepairState.java
@@ -1,4 +1,18 @@
 package States;
 public class RepairState {
-    
+    @Override
+    public void rentCar(CarContext carContext){
+
+    }
+
+    @Override
+    public void returnCar(CarContext carContext){
+        
+    }
+
+    @Override
+    public void repairCar(CarContext carContext) {
+        carContext.setState(new AvailableState());
+
+    }
 }
