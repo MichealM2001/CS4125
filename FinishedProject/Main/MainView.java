0283977c25ca3202aae6bfb1380e13970824f54e
Rian <rianboleary@gmail.com>
2023-12-04 19:54:54 +0000

    Edited files

--- FinishedProject/Main/MainView.java
+++ FinishedProject/Main/MainView.java
@@ -18,8 +18,6 @@ public class MainView {
 package Main;
 
 import Login.LoginGUI;
-import Cars.CarManagementGUI;
-import Cars.CarRentalGUI;
 
 public class MainView {
     public static void main(String[] args) {
@@ -29,15 +27,6 @@ public class MainView {
         if(mainController.checkLoginStatus()){
             // Go to home screen 
             mainController.goToHome();
-
-            // Display Car Management GUI
-            CarManagementGUI carManagement = new CarManagementGUI();
-            carManagement.setVisible(true);
-
-            // Display Car Rental GUI
-            CarRentalGUI carRental = new CarRentalGUI("username");
-            carRental.setVisible(true);
-
         }else{
             new LoginGUI().setVisible(true);
         }
