808183036a11dfcf7623ed005d4f0e8ae3cb52d3
Barry <20236263@studentmail.ul.ie>
2023-12-04 14:38:33 +0000

    Create LuxuryCarDecorator.java

---
+++ FinishedProject/Cars/LuxuryCarDecorator.java
@@ -0,0 +1,25 @@
+package Cars;
+
+// LuxuryCarDecorator class extends the functionality of CarDecorator
+public class LuxuryCarDecorator extends CarDecorator {
+
+    // Constructor for LuxuryCarDecorator
+    public LuxuryCarDecorator(Car decoratedCar) {
+        super(decoratedCar);
+    }
+
+    // Overriding the getPrice method to include the cost of luxury features
+    @Override
+    public int getPrice() {
+        // Assuming a fixed additional cost for luxury features. 
+        int luxuryFeatureCost = 5000;
+
+        // Return the original price plus the additional luxury feature cost
+        return decoratedCar.getPrice() + luxuryFeatureCost;
+    }
+
+    public String getLuxuryFeatures() {
+        // Return a string describing the luxury features
+        return "Premium leather interior, advanced infotainment system, enhanced safety features";
+    }
+}
